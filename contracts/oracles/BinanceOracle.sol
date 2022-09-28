c83e94b06e5a91a4e50e8374906e657c3d1aacc2
Narayan Prusty <arsenal.narayan@gmail.com>
2022-09-28 17:44:43 +0100

    added binance oracle

--- contracts/oracles/BinanceOracle.sol
+++ contracts/oracles/BinanceOracle.sol
@@ -2,15 +2,21 @@
 pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "../interfaces/OracleInterface.sol";
+import "../interfaces/FeedRegistryInterface.sol";
+import "../interfaces/VBep20Interface.sol";
+import "../interfaces/BEP20Interface.sol";
 
-contract BinanceOracle is OracleInterface, Initializable {
+contract BinanceOracle is Initializable {
+
+    FeedRegistryInterface public feedRegistry;
     
-    function initialize() public initializer {
-
+    function initialize(FeedRegistryInterface feed) public initializer {
+        feedRegistry = feed;
     }
 
-    function getUnderlyingPrice(address vToken) external view override returns (uint256) {
-        return 0;
+    function getUnderlyingPrice(VBep20Interface vToken) public view returns (uint256) {
+        BEP20Interface underlyingToken = BEP20Interface(vToken.underlying());
+        (,int256 answer,,,) = feedRegistry.latestRoundDataByName(underlyingToken.symbol(), "USD");
+        return uint256(answer);
     }
 }
