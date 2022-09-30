947876f627a3cbc3b935ff686b7f669c929efe3c
narayanprusty <arsenal.narayan@gmail.com>
2022-09-30 18:50:41 +0100

    fix test

--- contracts/oracles/ChainlinkOracle.sol
+++ contracts/oracles/ChainlinkOracle.sol
@@ -112,7 +112,8 @@ contract ChainlinkOracle is OwnableUpgradeable, OracleInterface {
 
         (, int256 answer, , uint256 updatedAt, ) = feed.latestRoundData();
         require(answer > 0, "chainlink price must be positive");
-
+        
+        require(block.timestamp > updatedAt, "updatedAt exceeds block time");
         uint256 deltaTime = block.timestamp - updatedAt;
         require(deltaTime <= maxStalePeriod, "chainlink price expired");
 
