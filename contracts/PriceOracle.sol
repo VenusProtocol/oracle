c8ad86a3363eaa54e20cc48f327a6b69bbed8e8c
Narayan Prusty <arsenal.narayan@gmail.com>
2023-01-09 13:27:40 +0000

    removed unwanted var

--- contracts/PriceOracle.sol
+++ contracts/PriceOracle.sol
@@ -2,9 +2,6 @@
 pragma solidity 0.8.13;
 
 abstract contract PriceOracle {
-    /// @notice Indicator that this is a PriceOracle contract (for inspection)
-    bool public constant isPriceOracle = true;
-
     /**
      * @notice Get the underlying price of a vToken asset
      * @param vToken The vToken address to get the underlying price of
