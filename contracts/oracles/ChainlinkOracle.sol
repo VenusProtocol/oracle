906c7aedecff815461f1df6bce572c210790a2aa
Narayan Prusty <arsenal.narayan@gmail.com>
2023-01-09 13:06:48 +0000

    remove todo

--- contracts/oracles/ChainlinkOracle.sol
+++ contracts/oracles/ChainlinkOracle.sol
@@ -28,8 +28,7 @@ contract ChainlinkOracle is OwnableUpgradeable, OracleInterface {
     /// @notice Set this as asset address for BNB. This is the underlying for vBNB
     address public constant BNB_ADDR = 0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB;
 
-    /// @notice TODO: might be removed some day, it's for enabling us to force set the prices to
-    /// certain values in some urgent conditions
+    /// @notice Enables us to force set the prices to certain values in some urgent conditions
     mapping(address => uint256) public prices;
 
     /// @notice token config by assets
@@ -74,7 +73,6 @@ contract ChainlinkOracle is OwnableUpgradeable, OracleInterface {
 
         if (_compareStrings(symbol, "VAI")) {
             return VAI_VALUE;
-            // @TODO: This is some history code, keep it here in case of messing up
         } else {
             return _getUnderlyingPriceInternal(VBep20Interface(vToken));
         }
