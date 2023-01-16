e8b957789154b66b1150967cf168f9cc4a7e7cae
Corey Rice <coreyarice@gmail.com>
2023-01-15 22:43:50 -0300

    chore: update bound validator nat spec

--- contracts/oracles/BoundValidator.sol
+++ contracts/oracles/BoundValidator.sol
@@ -8,10 +8,12 @@ import "../interfaces/OracleInterface.sol";
 struct ValidateConfig {
     /// @notice asset address
     address asset;
-    /// @notice upper bound of deviation between reported price and anchor price,
+    
+    /// @notice Upper bound of deviation between reported price and anchor price,
     /// beyond which the reported price will be invalidated
     uint256 upperBoundRatio;
-    /// @notice lower bound of deviation between reported price and anchor price,
+    
+    /// @notice Lower bound of deviation between reported price and anchor price,
     /// below which the reported price will be invalidated
     uint256 lowerBoundRatio;
 }
@@ -29,14 +31,14 @@ contract BoundValidator is OwnableUpgradeable, BoundValidatorInterface {
     /// @notice Set this as asset address for BNB. This is the underlying for vBNB
     address public constant BNB_ADDR = 0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB;
 
-    /// @notice Emit this event when new validate configs are added
+    /// @notice Emit this event when new validation configs are added
     event ValidateConfigAdded(address indexed asset, uint256 indexed upperBound, uint256 indexed lowerBound);
 
     /// @notice Constructor for the implementation contract. Sets immutable variables.
-    /// @param vBnbAddress The address of the VBNB
+    /// @param vBnbAddress The address of the vBNB
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor(address vBnbAddress) {
-        require(vBnbAddress != address(0), "can't be zero address");
+        require(vBnbAddress != address(0), "can't be a zero address");
         vBnb = vBnbAddress;
         _disableInitializers();
     }
@@ -50,9 +52,10 @@ contract BoundValidator is OwnableUpgradeable, BoundValidatorInterface {
 
     /**
      * @notice Add multiple validation configs at the same time
-     * @param configs config array
+     * @param configs Array of validation configs
      * @custom:access Only Governance
-     * @custom:error Zero length error thrown, if length of the array in parameter is 0
+     * @custom:error Zero length error is thrown if length of the config array is 0
+     * @custom:event Emits ValidateConfigAdded for each validation config that is successfully set
      */
     function setValidateConfigs(ValidateConfig[] memory configs) external virtual onlyOwner {
         require(configs.length > 0, "invalid validate config length");
@@ -62,13 +65,13 @@ contract BoundValidator is OwnableUpgradeable, BoundValidatorInterface {
     }
 
     /**
-     * @notice Add single validation config
-     * @param config config struct
+     * @notice Add a single validation config
+     * @param config Validation config struct
      * @custom:access Only Governance
-     * @custom:error Zero address error thrown if asset address is zero
+     * @custom:error Null address error is thrown if asset address is null
      * @custom:error Range error thrown if bound ratio is not positive
      * @custom:error Range error thrown if lower bound is greater than upper bound
-     * @custom:event Emits ValidateConfigAdded if succesfully config are set
+     * @custom:event Emits ValidateConfigAdded when a validation config is successfully set
      */
     function setValidateConfig(ValidateConfig memory config) public virtual onlyOwner {
         require(config.asset != address(0), "asset can't be zero address");
@@ -81,31 +84,31 @@ contract BoundValidator is OwnableUpgradeable, BoundValidatorInterface {
     /**
      * @notice Test reported asset price against anchor price
      * @param vToken vToken address
-     * @param reporterPrice the price to be tested
+     * @param reportedPrice The price to be tested
      * @custom:error Missing error thrown if asset config is not set
-     * @custom:error Price error thrwon if anchor price is not valid
+     * @custom:error Price error thrown if anchor price is not valid
      */
     function validatePriceWithAnchorPrice(
         address vToken,
-        uint256 reporterPrice,
+        uint256 reportedPrice,
         uint256 anchorPrice
     ) public view virtual override returns (bool) {
         address asset = vToken == vBnb ? BNB_ADDR : VBep20Interface(vToken).underlying();
 
         require(validateConfigs[asset].upperBoundRatio != 0, "validation config not exist");
         require(anchorPrice != 0, "anchor price is not valid");
-        return _isWithinAnchor(asset, reporterPrice, anchorPrice);
+        return _isWithinAnchor(asset, reportedPrice, anchorPrice);
     }
 
     /**
-     * @notice Test whether the reported price is within the predefined bounds
-     * @param asset asset address
-     * @param reporterPrice the price to be tested
-     * @param anchorPrice anchor price as testing anchor
+     * @notice Test whether the reported price is within the valid bounds
+     * @param asset Asset address
+     * @param reportedPrice The price to be tested
+     * @param anchorPrice The reported price must be within the the valid bounds of this price
      */
-    function _isWithinAnchor(address asset, uint256 reporterPrice, uint256 anchorPrice) internal view returns (bool) {
-        if (reporterPrice > 0) {
-            uint256 anchorRatio = (anchorPrice * 100e16) / reporterPrice;
+    function _isWithinAnchor(address asset, uint256 reportedPrice, uint256 anchorPrice) internal view returns (bool) {
+        if (reportedPrice > 0) {
+            uint256 anchorRatio = (anchorPrice * 100e16) / reportedPrice;
             uint256 upperBoundAnchorRatio = validateConfigs[asset].upperBoundRatio;
             uint256 lowerBoundAnchorRatio = validateConfigs[asset].lowerBoundRatio;
             return anchorRatio <= upperBoundAnchorRatio && anchorRatio >= lowerBoundAnchorRatio;
