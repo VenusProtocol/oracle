bd33074e181631a1742e073814fe87bd91d262d0
Narayan Prusty <arsenal.narayan@gmail.com>
2023-04-17 12:08:11 +0100

    remove import

--- contracts/oracles/BinanceOracle.sol
+++ contracts/oracles/BinanceOracle.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity 0.8.13;
 
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "../interfaces/VBep20Interface.sol";
 import "../interfaces/SIDRegistryInterface.sol";
