ccf31f27d423dcab393de3f4f8266c2d21f29828
Narayan Prusty <arsenal.narayan@gmail.com>
2023-05-26 14:05:48 +0100

    chore: uncomment configure

--- deploy/2-configure-feeds.ts
+++ deploy/2-configure-feeds.ts
@@ -234,100 +234,100 @@ const addr0000 = "0x0000000000000000000000000000000000000000";
 const DEFAULT_STALE_PERIOD = 24 * 60 * 60; // 24 hrs
 
 const func: DeployFunction = async function ({ network, deployments, getNamedAccounts }: HardhatRuntimeEnvironment) {
-  // const networkName: string = network.name === "bscmainnet" ? "bscmainnet" : "bsctestnet";
-  // const { deploy } = deployments;
-  // const { deployer } = await getNamedAccounts();
+  const networkName: string = network.name === "bscmainnet" ? "bscmainnet" : "bsctestnet";
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
 
-  // const resilientOracle = await hre.ethers.getContract("ResilientOracle");
-  // const binanceOracle = await hre.ethers.getContract("BinanceOracle");
-  // const chainlinkOracle = await hre.ethers.getContract("ChainlinkOracle");
-  // const pythOracle = await hre.ethers.getContract("PythOracle");
+  const resilientOracle = await hre.ethers.getContract("ResilientOracle");
+  const binanceOracle = await hre.ethers.getContract("BinanceOracle");
+  const chainlinkOracle = await hre.ethers.getContract("ChainlinkOracle");
+  const pythOracle = await hre.ethers.getContract("PythOracle");
 
-  // const oraclesData: Oracles = {
-  //   chainlink: {
-  //     oracles: [chainlinkOracle.address, addr0000, addr0000],
-  //     enableFlagsForOracles: [true, false, false],
-  //     underlyingOracle: chainlinkOracle,
-  //     getTokenConfig: (asset: Asset, name: string) => ({
-  //       asset: asset.address,
-  //       feed: chainlinkFeed[name][asset.token],
-  //       maxStalePeriod: DEFAULT_STALE_PERIOD,
-  //     }),
-  //   },
-  //   binance: {
-  //     oracles: [binanceOracle.address, addr0000, addr0000],
-  //     enableFlagsForOracles: [true, false, false],
-  //     underlyingOracle: binanceOracle,
-  //     getStalePeriodConfig: (asset: Asset) => [asset.token, DEFAULT_STALE_PERIOD.toString()],
-  //   },
-  //   pyth: {
-  //     oracles: [pythOracle.address, addr0000, addr0000],
-  //     enableFlagsForOracles: [true, false, false],
-  //     underlyingOracle: pythOracle,
-  //     getTokenConfig: (asset: Asset, name: string) => ({
-  //       pythId: pythID[name][asset.token],
-  //       asset: asset.address,
-  //       maxStalePeriod: DEFAULT_STALE_PERIOD,
-  //     }),
-  //   },
-  // };
+  const oraclesData: Oracles = {
+    chainlink: {
+      oracles: [chainlinkOracle.address, addr0000, addr0000],
+      enableFlagsForOracles: [true, false, false],
+      underlyingOracle: chainlinkOracle,
+      getTokenConfig: (asset: Asset, name: string) => ({
+        asset: asset.address,
+        feed: chainlinkFeed[name][asset.token],
+        maxStalePeriod: DEFAULT_STALE_PERIOD,
+      }),
+    },
+    binance: {
+      oracles: [binanceOracle.address, addr0000, addr0000],
+      enableFlagsForOracles: [true, false, false],
+      underlyingOracle: binanceOracle,
+      getStalePeriodConfig: (asset: Asset) => [asset.token, DEFAULT_STALE_PERIOD.toString()],
+    },
+    pyth: {
+      oracles: [pythOracle.address, addr0000, addr0000],
+      enableFlagsForOracles: [true, false, false],
+      underlyingOracle: pythOracle,
+      getTokenConfig: (asset: Asset, name: string) => ({
+        pythId: pythID[name][asset.token],
+        asset: asset.address,
+        maxStalePeriod: DEFAULT_STALE_PERIOD,
+      }),
+    },
+  };
 
-  // for (const asset of assets[networkName]) {
-  //   const { oracle } = asset;
-  //   console.log(`Configuring ${asset.token}`);
+  for (const asset of assets[networkName]) {
+    const { oracle } = asset;
+    console.log(`Configuring ${asset.token}`);
 
-  //   if (network.live) {
-  //     console.log(`Configuring ${oracle} oracle for ${asset.token}`);
+    if (network.live) {
+      console.log(`Configuring ${oracle} oracle for ${asset.token}`);
 
-  //     const { getTokenConfig } = oraclesData[oracle];
-  //     if (oraclesData[oracle].underlyingOracle.address !== binanceOracle.address && getTokenConfig !== undefined) {
-  //       const tx = await oraclesData[oracle].underlyingOracle?.setTokenConfig(getTokenConfig(asset, networkName));
-  //       tx.wait(1);
-  //     }
+      const { getTokenConfig } = oraclesData[oracle];
+      if (oraclesData[oracle].underlyingOracle.address !== binanceOracle.address && getTokenConfig !== undefined) {
+        const tx = await oraclesData[oracle].underlyingOracle?.setTokenConfig(getTokenConfig(asset, networkName));
+        tx.wait(1);
+      }
 
-  //     const { getStalePeriodConfig } = oraclesData[oracle];
-  //     if (
-  //       oraclesData[oracle].underlyingOracle.address === binanceOracle.address &&
-  //       getStalePeriodConfig !== undefined
-  //     ) {
-  //       const tx = await oraclesData[oracle].underlyingOracle?.setTokenConfig(...getStalePeriodConfig(asset));
-  //       tx.wait(1);
-  //     }
+      const { getStalePeriodConfig } = oraclesData[oracle];
+      if (
+        oraclesData[oracle].underlyingOracle.address === binanceOracle.address &&
+        getStalePeriodConfig !== undefined
+      ) {
+        const tx = await oraclesData[oracle].underlyingOracle?.setTokenConfig(...getStalePeriodConfig(asset));
+        tx.wait(1);
+      }
 
-  //     console.log(`Configuring resillient oracle for ${asset.token}`);
-  //     const tx = await resilientOracle.setTokenConfig({
-  //       asset: asset.address,
-  //       oracles: oraclesData[oracle].oracles,
-  //       enableFlagsForOracles: oraclesData[oracle].enableFlagsForOracles,
-  //     });
+      console.log(`Configuring resillient oracle for ${asset.token}`);
+      const tx = await resilientOracle.setTokenConfig({
+        asset: asset.address,
+        oracles: oraclesData[oracle].oracles,
+        enableFlagsForOracles: oraclesData[oracle].enableFlagsForOracles,
+      });
 
-  //     await tx.wait(1);
-  //   } else {
-  //     await deploy(`Mock${asset.token}`, {
-  //       from: deployer,
-  //       log: true,
-  //       deterministicDeployment: false,
-  //       args: [`Mock${asset.token}`, `Mock${asset.token}`, 18],
-  //       autoMine: true,
-  //       contract: "BEP20Harness",
-  //     });
+      await tx.wait(1);
+    } else {
+      await deploy(`Mock${asset.token}`, {
+        from: deployer,
+        log: true,
+        deterministicDeployment: false,
+        args: [`Mock${asset.token}`, `Mock${asset.token}`, 18],
+        autoMine: true,
+        contract: "BEP20Harness",
+      });
 
-  //     const mock = await hre.ethers.getContract(`Mock${asset.token}`);
+      const mock = await hre.ethers.getContract(`Mock${asset.token}`);
 
-  //     console.log(`Configuring resillient oracle for ${asset.token}`);
-  //     let tx = await resilientOracle.setTokenConfig({
-  //       asset: mock.address,
-  //       oracles: oraclesData[oracle].oracles,
-  //       enableFlagsForOracles: oraclesData[oracle].enableFlagsForOracles,
-  //     });
+      console.log(`Configuring resillient oracle for ${asset.token}`);
+      let tx = await resilientOracle.setTokenConfig({
+        asset: mock.address,
+        oracles: oraclesData[oracle].oracles,
+        enableFlagsForOracles: oraclesData[oracle].enableFlagsForOracles,
+      });
 
-  //     await tx.wait(1);
+      await tx.wait(1);
 
-  //     console.log(`Configuring ${oracle} oracle for ${asset.token}`);
-  //     tx = await oraclesData[oracle].underlyingOracle?.setPrice(mock.address, asset.price);
-  //     await tx.wait(1);
-  //   }
-  // }
+      console.log(`Configuring ${oracle} oracle for ${asset.token}`);
+      tx = await oraclesData[oracle].underlyingOracle?.setPrice(mock.address, asset.price);
+      await tx.wait(1);
+    }
+  }
 };
 
 export default func;
