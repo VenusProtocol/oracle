85f7c3ff1dc8e961a7f73355ac01907fba6fd5b6
Narayan Prusty <arsenal.narayan@gmail.com>
2024-03-04 11:42:14 +0000

    fix: added OneJumpOracle

---
+++ contracts/oracles/OneJumpOracle.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity 0.8.13;
+
+import { CorrelatedTokenOracle } from "./common/CorrelatedTokenOracle.sol";
+import { ensureNonzeroAddress } from "@venusprotocol/solidity-utilities/contracts/validators.sol";
+import { OracleInterface } from "../interfaces/OracleInterface.sol";
+
+/**
+ * @title OneJumpOracle
+ * @author Venus
+ * @notice This oracle fetches the price of an asset in through an intermediate asset
+ */
+contract OneJumpOracle is CorrelatedTokenOracle {
+    /// @notice Address of the PT oracle
+    /// @custom:oz-upgrades-unsafe-allow state-variable-immutable
+    OracleInterface public immutable INTERMEDIATE_ORACLE;
+
+    /// @notice Constructor for the implementation contract.
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor(
+        address correlatedToken,
+        address underlyingToken,
+        address resilientOracle,
+        address intermediateOracle
+    ) CorrelatedTokenOracle(correlatedToken, underlyingToken, resilientOracle) {
+        ensureNonzeroAddress(intermediateOracle);
+        INTERMEDIATE_ORACLE = OracleInterface(intermediateOracle);
+    }
+
+    /**
+     * @notice Fetches the amount of the underlying token for 1 correlated token
+     * @return amount The amount of the underlying token for 1 correlated token
+     */
+    function getUnderlyingAmount() internal view override returns (uint256) {
+        return INTERMEDIATE_ORACLE.getPrice(CORRELATED_TOKEN);
+    }
+}
---
+++ test/OneJumpOracle.ts
@@ -0,0 +1,95 @@
+import { smock } from "@defi-wonderland/smock";
+import chai from "chai";
+import { parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+
+import { BEP20Harness, OracleInterface, ResilientOracleInterface } from "../typechain-types";
+import { addr0000 } from "./utils/data";
+
+const { expect } = chai;
+chai.use(smock.matchers);
+
+const LDO_ETH_PRICE = parseUnits("0.000945180903526149", 18); // 3.30 USD for 1 LDO
+const ETH_USD_PRICE = parseUnits("3496.14", 18); // 3,496.14 USD for 1 ETH
+
+describe("OneJumpOracle unit tests", () => {
+  let ldoMock;
+  let wethMock;
+  let resilientOracleMock;
+  let OneJumpOracleFactory;
+  let OneJumpOracle;
+  let chainlinkOracleMock;
+  before(async () => {
+    //  To initialize the provider we need to hit the node with any request
+    await ethers.getSigners();
+    resilientOracleMock = await smock.fake<ResilientOracleInterface>("ResilientOracleInterface");
+    chainlinkOracleMock = await smock.fake<OracleInterface>("OracleInterface");
+
+    ldoMock = await smock.fake<BEP20Harness>("BEP20Harness");
+    ldoMock.decimals.returns(18);
+
+    wethMock = await smock.fake<BEP20Harness>("BEP20Harness");
+    wethMock.decimals.returns(18);
+
+    resilientOracleMock.getPrice.whenCalledWith(wethMock.address).returns(ETH_USD_PRICE.toString());
+    chainlinkOracleMock.getPrice.whenCalledWith(ldoMock.address).returns(LDO_ETH_PRICE.toString());
+
+    OneJumpOracleFactory = await ethers.getContractFactory("OneJumpOracle");
+  });
+
+  describe("deployment", () => {
+    it("revert if correlated token address is 0", async () => {
+      await expect(
+        OneJumpOracleFactory.deploy(
+          addr0000,
+          wethMock.address,
+          resilientOracleMock.address,
+          chainlinkOracleMock.address,
+        ),
+      ).to.be.reverted;
+    });
+
+    it("revert if underlying token address is 0", async () => {
+      await expect(
+        OneJumpOracleFactory.deploy(
+          ldoMock.address,
+          addr0000,
+          resilientOracleMock.address,
+          chainlinkOracleMock.address,
+        ),
+      ).to.be.reverted;
+    });
+
+    it("revert if resilient oracle address is 0", async () => {
+      await expect(
+        OneJumpOracleFactory.deploy(ldoMock.address, wethMock.address, addr0000, chainlinkOracleMock.address),
+      ).to.be.reverted;
+    });
+
+    it("revert if intermediate oracle address is 0", async () => {
+      await expect(
+        OneJumpOracleFactory.deploy(ldoMock.address, wethMock.address, resilientOracleMock.address, addr0000),
+      ).to.be.reverted;
+    });
+
+    it("should deploy contract", async () => {
+      OneJumpOracle = await OneJumpOracleFactory.deploy(
+        ldoMock.address,
+        wethMock.address,
+        resilientOracleMock.address,
+        chainlinkOracleMock.address,
+      );
+    });
+  });
+
+  describe("getPrice", () => {
+    it("revert if address is not valid LDO address", async () => {
+      await expect(OneJumpOracle.getPrice(addr0000)).to.be.revertedWith("wrong token address");
+    });
+
+    it("should get correct price of LDO", async () => {
+      const price = await OneJumpOracle.getPrice(ldoMock.address);
+      expect(price).to.equal(parseUnits("3.304484764053910564", 18));
+    });
+  });
+});
