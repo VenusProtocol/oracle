ac18ad7bdf47290555b2040cb6e29b6d49f12458
Narayan Prusty <arsenal.narayan@gmail.com>
2024-03-18 12:33:00 +0530

    fix: VPB-01

--- contracts/oracles/WeETHOracle.sol
+++ contracts/oracles/WeETHOracle.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity 0.8.13;
 
-import { IWeETH } from "../interfaces/IWeETH.sol";
 import { CorrelatedTokenOracle } from "./common/CorrelatedTokenOracle.sol";
+import { IEtherFiLiquidityPool } from "../interfaces/IEtherFiLiquidityPool.sol";
 import { EXP_SCALE } from "@venusprotocol/solidity-utilities/contracts/constants.sol";
+import { ensureNonzeroAddress } from "@venusprotocol/solidity-utilities/contracts/validators.sol";
 
 /**
  * @title WeETHOracle
@@ -11,19 +12,27 @@ import { EXP_SCALE } from "@venusprotocol/solidity-utilities/contracts/constants
  * @notice This oracle fetches the price of weETH
  */
 contract WeETHOracle is CorrelatedTokenOracle {
+    /// @notice Address of Liqiudity pool
+    /// @custom:oz-upgrades-unsafe-allow state-variable-immutable
+    IEtherFiLiquidityPool public immutable LIQUIDITY_POOL;
+
     /// @notice Constructor for the implementation contract.
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor(
+        address liquidityPool,
         address weETH,
         address eETH,
         address resilientOracle
-    ) CorrelatedTokenOracle(weETH, eETH, resilientOracle) {}
+    ) CorrelatedTokenOracle(weETH, eETH, resilientOracle) {
+        ensureNonzeroAddress(liquidityPool);
+        LIQUIDITY_POOL = IEtherFiLiquidityPool(liquidityPool);
+    }
 
     /**
      * @notice Gets the eETH for 1 weETH
      * @return amount Amount of eETH
      */
     function getUnderlyingAmount() internal view override returns (uint256) {
-        return IWeETH(CORRELATED_TOKEN).getEETHByWeETH(EXP_SCALE);
+        return LIQUIDITY_POOL.amountForShare(EXP_SCALE);
     }
 }
