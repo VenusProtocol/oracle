fc52a39b0033916df264551d5364ed43f1ca79da
Narayan Prusty <arsenal.narayan@gmail.com>
2024-05-08 16:47:01 +0530

    fix: use sfrxEthFraxOracle oracle

--- contracts/oracles/SFrxETHOracle.sol
+++ contracts/oracles/SFrxETHOracle.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity 0.8.25;
 
-import { ISfrxETH } from "../interfaces/ISfrxETH.sol";
 import { CorrelatedTokenOracle } from "./common/CorrelatedTokenOracle.sol";
+import { ISfrxEthFraxOracle } from "../interfaces/ISfrxEthFraxOracle.sol";
+import { ensureNonzeroAddress } from "@venusprotocol/solidity-utilities/contracts/validators.sol";
 import { EXP_SCALE } from "@venusprotocol/solidity-utilities/contracts/constants.sol";
 
 /**
@@ -11,19 +12,38 @@ import { EXP_SCALE } from "@venusprotocol/solidity-utilities/contracts/constants
  * @notice This oracle fetches the price of sfrxETH
  */
 contract SFrxETHOracle is CorrelatedTokenOracle {
+    /// @notice Address of SfrxEthFraxOracle
+    /// @custom:oz-upgrades-unsafe-allow state-variable-immutable
+    ISfrxEthFraxOracle public immutable SFRXETH_FRAX_ORACLE;
+
+    /// @notice Thrown if the price data is invalid
+    error BadPriceData();
+
     /// @notice Constructor for the implementation contract.
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor(
+        address sfrxEthFraxOracle,
         address sfrxETH,
-        address frxETH,
+        address frax,
         address resilientOracle
-    ) CorrelatedTokenOracle(sfrxETH, frxETH, resilientOracle) {}
+    ) CorrelatedTokenOracle(sfrxETH, frax, resilientOracle) {
+        ensureNonzeroAddress(sfrxEthFraxOracle);
+        SFRXETH_FRAX_ORACLE = ISfrxEthFraxOracle(sfrxEthFraxOracle);
+    }
 
     /**
-     * @notice Gets the frxETH for 1 sfrxETH
-     * @return amount Amount of frxETH
+     * @notice Gets the FRAX for 1 sfrxETH
+     * @return amount Amount of FRAX
      */
     function _getUnderlyingAmount() internal view override returns (uint256) {
-        return ISfrxETH(CORRELATED_TOKEN).convertToAssets(EXP_SCALE);
+        (bool isBadData, uint256 priceLow, uint256 priceHigh) = SFRXETH_FRAX_ORACLE.getPrices();
+
+        if (isBadData) revert BadPriceData();
+        
+        // calculate average price
+        uint256 averagePrice = (priceLow + priceHigh) / 2;
+
+        // return (1 / averagePrice) as the average price is in sfraxETH
+        return EXP_SCALE * 2 / averagePrice;
     }
 }
