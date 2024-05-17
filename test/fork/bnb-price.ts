b2908f52281d721dee3bd36f4c35019e6da61701
Narayan Prusty <arsenal.narayan@gmail.com>
2024-05-17 15:57:53 +0530

    fix: added bnb test

--- test/fork/bnb-price.ts
+++ test/fork/bnb-price.ts
@@ -17,9 +17,9 @@ const RESILIENT_ORACLE_ADDRESS = "0x6592b5DE802159F3E74B2486b091D11a8256ab8A";
 const CHAINLINK_ORACLE = "0x1B2103441A0A108daD8848D8F5d790e4D402921F";
 const REDSTONE_ORACLE = "0x8455EFA4D7Ff63b8BFD96AdD889483Ea7d39B70a";
 const NORMAL_TIMELOCK = "0x939bD8d64c0A9583A7Dcea9933f7b21697ab6396";
-const BTC = "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c";
-const REDSTONE_BTC_FEED = "0xa51738d1937FFc553d5070f43300B385AA2D9F55";
-const REDSTONE_BTC_STALE_PERIOD = 60 * 60; // 1 hour
+const BNB = "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB";
+const REDSTONE_BNB_FEED = "0x8dd2D85C7c28F43F965AE4d9545189C7D022ED0e";
+const REDSTONE_BNB_STALE_PERIOD = 60 * 60; // 1 hour
 const BOUND_VALIFATOR = "0x6E332fF0bB52475304494E4AE5063c1051c7d735";
 
 if (FORK && FORKED_NETWORK === "bscmainnet") {
@@ -55,43 +55,46 @@ if (FORK && FORKED_NETWORK === "bscmainnet") {
           await ethers.getSigner(NORMAL_TIMELOCK),
         );
 
-        // add BTC feed to redstone oracle
+        // add BNB feed to redstone oracle
         await redstoneOracle.setTokenConfig({
-          asset: BTC,
-          feed: REDSTONE_BTC_FEED,
-          maxStalePeriod: REDSTONE_BTC_STALE_PERIOD,
+          asset: BNB,
+          feed: REDSTONE_BNB_FEED,
+          maxStalePeriod: REDSTONE_BNB_STALE_PERIOD,
         });
 
         // add main, pivot and fallback oracle
         await resilientOracle.setTokenConfig({
-          asset: BTC,
+          asset: BNB,
           oracles: [REDSTONE_ORACLE, CHAINLINK_ORACLE, CHAINLINK_ORACLE],
           enableFlagsForOracles: [true, true, true],
         });
 
         // set upper and lower bounds for price to be valid - we are setting price to be within 1% difference
         await boundValdiator.setValidateConfig({
-          asset: BTC,
+          asset: BNB,
           upperBoundRatio: parseUnits("1.01", 18),
           lowerBoundRatio: parseUnits("0.99", 18),
         });
+
+        console.log((await redstoneOracle.getPrice(BNB)).toString());
+        console.log((await chainlinkOracle.getPrice(BNB)).toString());
       });
 
       it("Validate price", async () => {
-        const redstonePrice = await redstoneOracle.getPrice(BTC);
-        const resilientPrice = await resilientOracle.getPrice(BTC);
+        const redstonePrice = await redstoneOracle.getPrice(BNB);
+        const resilientPrice = await resilientOracle.getPrice(BNB);
         expect(resilientPrice).to.be.eq(redstonePrice);
       });
 
       it("Invalidate price and use fallback", async () => {
         await boundValdiator.setValidateConfig({
-          asset: BTC,
+          asset: BNB,
           upperBoundRatio: parseUnits("1.00000001", 18),
           lowerBoundRatio: parseUnits("1", 18),
         });
 
-        const chainlinkPrice = await chainlinkOracle.getPrice(BTC);
-        const resilientPrice = await resilientOracle.getPrice(BTC);
+        const chainlinkPrice = await chainlinkOracle.getPrice(BNB);
+        const resilientPrice = await resilientOracle.getPrice(BNB);
         expect(resilientPrice).to.be.eq(chainlinkPrice);
       });
     });
