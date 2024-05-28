d58ee27973c892a68733f3ca8707c361a93f2fbd
Narayan Prusty <arsenal.narayan@gmail.com>
2024-05-28 13:43:39 +0400

    fix: added deploy script

---
+++ deploy/8-deploy-sfrxeth-oracle.ts
@@ -0,0 +1,51 @@
+import { parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+import { DeployFunction } from "hardhat-deploy/dist/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+import { ADDRESSES } from "../helpers/deploymentConfig";
+
+const func: DeployFunction = async ({ getNamedAccounts, deployments, network }: HardhatRuntimeEnvironment) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  const proxyOwnerAddress = network.live ? ADDRESSES[network.name].timelock : deployer;
+  const { SfrxEthFraxOracle, sfrxETH } = ADDRESSES[network.name];
+  const maxAllowedPriceDifference = parseUnits("100", 18);
+
+  let SfrxEthFraxOracleAddress = SfrxEthFraxOracle;
+  if (!SfrxEthFraxOracleAddress) {
+    await deploy("MockSfrxEthFraxOracle", {
+      contract: "MockSfrxEthFraxOracle",
+      from: deployer,
+      log: true,
+      autoMine: true,
+      skipIfAlreadyDeployed: true,
+      args: [],
+    });
+
+    const mockSfrxEthFraxOracleContract = await ethers.getContract("MockSfrxEthFraxOracle");
+    SfrxEthFraxOracleAddress = mockSfrxEthFraxOracleContract.address;
+
+    if ((await mockSfrxEthFraxOracleContract.owner()) === deployer) {
+      await mockSfrxEthFraxOracleContract.transferOwnership(proxyOwnerAddress);
+    }
+  }
+
+  await deploy("SFrxETHOracle", {
+    contract: "SFrxETHOracle",
+    from: deployer,
+    log: true,
+    deterministicDeployment: false,
+    args: [SfrxEthFraxOracleAddress, sfrxETH, maxAllowedPriceDifference],
+    proxy: {
+      owner: proxyOwnerAddress,
+      proxyContract: "OptimizedTransparentProxy",
+    },
+    skipIfAlreadyDeployed: true,
+  });
+};
+
+export default func;
+func.tags = ["sFraxOracle"];
+func.skip = async (hre: HardhatRuntimeEnvironment) => hre.network.name !== "ethereum" && hre.network.name !== "sepolia";
