7df781130f0481fce823854713c73595509957c8
Narayan Prusty <arsenal.narayan@gmail.com>
2024-07-05 17:35:22 +0530

    fix: updated rsETH oracle

--- deploy/11-deploy-rsETH-oracles.ts
+++ deploy/11-deploy-rsETH-oracles.ts
@@ -5,20 +5,20 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { ADDRESSES } from "../helpers/deploymentConfig";
 
 const func: DeployFunction = async function ({ getNamedAccounts, deployments, network }: HardhatRuntimeEnvironment) {
+  const networkName: string = network.name === "hardhat" ? "sepolia" : network.name;
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
-  const proxyOwnerAddress = network.live ? ADDRESSES[network.name].timelock : deployer;
+  const proxyOwnerAddress = network.live ? ADDRESSES[networkName].timelock : deployer;
   let WETH;
   let rsETH;
-  if (network.name === "sepolia" || network.name === "ethereum") {
-    ({ WETH } = ADDRESSES[network.name]);
+  if (networkName === "sepolia" || networkName === "ethereum") {
+    ({ WETH } = ADDRESSES[networkName]);
     rsETH =
-      network.name === "sepolia"
+    networkName === "sepolia"
         ? "0xfA0614E5C803E15070d31f7C38d2d430EBe68E47"
         : "0xA1290d69c65A6Fe4DF752f95823fae25cB99e5A7";
   }
 
-  console.log(`Timelock (${proxyOwnerAddress})`);
   const redStoneOracle = await hre.ethers.getContract("RedStoneOracle");
   const resilientOracle = await hre.ethers.getContract("ResilientOracle");
   const chainlinkOracle = await hre.ethers.getContract("ChainlinkOracle");
