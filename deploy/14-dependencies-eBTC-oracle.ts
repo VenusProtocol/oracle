90203968c3269b93a6120522bfc8d01453d598c0
web3rover <arsenal.narayan@gmail.com>
2024-10-30 16:35:49 +0400

    fix: fixed lint

--- deploy/14-dependencies-eBTC-oracle.ts
+++ deploy/14-dependencies-eBTC-oracle.ts
@@ -1,28 +1,28 @@
-// import { ethers } from "hardhat";
-// import { DeployFunction } from "hardhat-deploy/dist/types";
-// import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { ethers } from "hardhat";
+import { DeployFunction } from "hardhat-deploy/dist/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
 
-// import { ADDRESSES } from "../helpers/deploymentConfig";
+import { ADDRESSES } from "../helpers/deploymentConfig";
 
-// const func: DeployFunction = async ({ getNamedAccounts, deployments, network }: HardhatRuntimeEnvironment) => {
-//   const { deploy } = deployments;
-//   const { deployer } = await getNamedAccounts();
+const func: DeployFunction = async ({ getNamedAccounts, deployments, network }: HardhatRuntimeEnvironment) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
 
-//   const proxyOwnerAddress = network.live ? ADDRESSES[network.name].timelock : deployer;
+  const proxyOwnerAddress = network.live ? ADDRESSES[network.name].timelock : deployer;
 
-//   await deploy("MockAccountant_eBTC", {
-//     from: deployer,
-//     contract: "MockAccountant",
-//     args: [],
-//     log: true,
-//     autoMine: true,
-//     skipIfAlreadyDeployed: true,
-//   });
+  await deploy("MockAccountant_eBTC", {
+    from: deployer,
+    contract: "MockAccountant",
+    args: [],
+    log: true,
+    autoMine: true,
+    skipIfAlreadyDeployed: true,
+  });
 
-//   const mockAccountant = await ethers.getContract("MockAccountant_eBTC");
-//   await mockAccountant.transferOwnership(proxyOwnerAddress);
-// };
+  const mockAccountant = await ethers.getContract("MockAccountant_eBTC");
+  await mockAccountant.transferOwnership(proxyOwnerAddress);
+};
 
-// export default func;
-// func.tags = ["eBTCAccountantOracle"];
-// func.skip = async (hre: HardhatRuntimeEnvironment) => hre.network.name !== "sepolia";
+export default func;
+func.tags = ["eBTCAccountantOracle"];
+func.skip = async (hre: HardhatRuntimeEnvironment) => hre.network.name !== "sepolia";
