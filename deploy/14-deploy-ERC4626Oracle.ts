cbc04b7ef83eb732a897630a096fdcc5304662bb
GitGuru7 <128375421+GitGuru7@users.noreply.github.com>
2024-12-18 19:23:11 +0530

    feat: add config of Ethena's oracles on ethereum

--- deploy/14-deploy-ERC4626Oracle.ts
+++ deploy/14-deploy-ERC4626Oracle.ts
@@ -2,12 +2,12 @@ import { ethers } from "hardhat";
 import { DeployFunction } from "hardhat-deploy/dist/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
-const func: DeployFunction = async function ({ getNamedAccounts, deployments }: HardhatRuntimeEnvironment) {
+import { ADDRESSES } from "../helpers/deploymentConfig";
+
+const func: DeployFunction = async function ({ getNamedAccounts, deployments, network }: HardhatRuntimeEnvironment) {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
-
-  const sUSDe = "0xA3A3e5ecEA56940a4Ae32d0927bfd8821DdA848A";
-  const USDe = "0x8bAe3E12870a002A0D4b6Eb0F0CBf91b29d9806F";
+  const { sUSDe, USDe } = ADDRESSES[network.name];
 
   const resilientOracle = await ethers.getContract("ResilientOracle");
   await deploy("sUSDe_ERC4626Oracle", {
