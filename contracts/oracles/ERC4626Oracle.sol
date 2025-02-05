058701eaafbc7e093518a665ea5dc67a4ce8e285
web3rover <arsenal.narayan@gmail.com>
2025-02-05 13:29:01 +0400

    feat: redeployed sepolia and ethereum

--- contracts/oracles/ERC4626Oracle.sol
+++ contracts/oracles/ERC4626Oracle.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.25;
 
 import { IERC4626 } from "../interfaces/IERC4626.sol";
 import { CorrelatedTokenOracle } from "./common/CorrelatedTokenOracle.sol";
-import { EXP_SCALE } from "@venusprotocol/solidity-utilities/contracts/constants.sol";
 
 /**
  * @title ERC4626Oracle
@@ -11,19 +10,23 @@ import { EXP_SCALE } from "@venusprotocol/solidity-utilities/contracts/constants
  * @notice This oracle fetches the price of ERC4626 tokens
  */
 contract ERC4626Oracle is CorrelatedTokenOracle {
+    uint256 public immutable EXP_SCALE;
+
     /// @notice Constructor for the implementation contract.
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor(
         address correlatedToken,
         address underlyingToken,
         address resilientOracle
-    ) CorrelatedTokenOracle(correlatedToken, underlyingToken, resilientOracle) {}
+    ) CorrelatedTokenOracle(correlatedToken, underlyingToken, resilientOracle) {
+        EXP_SCALE = 10 ** IERC4626(correlatedToken).decimals();
+    }
 
     /**
      * @notice Fetches the amount of underlying token for 1 correlated token
      * @return amount The amount of underlying token for correlated token
      */
     function _getUnderlyingAmount() internal view override returns (uint256) {
-        return IERC4626(CORRELATED_TOKEN).convertToAssets(10 ** IERC4626(CORRELATED_TOKEN).decimals());
+        return IERC4626(CORRELATED_TOKEN).convertToAssets(EXP_SCALE);
     }
 }
