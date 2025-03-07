3069ded2911cd65b29e29a47374c460f6e1a0c5f
web3rover <arsenal.narayan@gmail.com>
2025-03-07 19:17:23 +0400

    fix: update bnb oracles

--- deploy/5-deploy-bnb-lst-oracles.ts
+++ deploy/5-deploy-bnb-lst-oracles.ts
@@ -4,79 +4,92 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 import { ADDRESSES, assets } from "../helpers/deploymentConfig";
 
-const func: DeployFunction = async ({
-  getNamedAccounts,
-  deployments,
-  network,
-  artifacts,
-}: HardhatRuntimeEnvironment) => {
+const func: DeployFunction = async ({ getNamedAccounts, deployments, network }: HardhatRuntimeEnvironment) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
   const oracle = await ethers.getContract("ResilientOracle");
-  const proxyOwnerAddress = network.live ? ADDRESSES[network.name].timelock : deployer;
 
   const { ankrBNB, stkBNB, BNBx, BNBxStakeManager, slisBNBStakeManager, stkBNBStakePool, slisBNB, wBETH } =
     ADDRESSES[network.name];
   const ETH = assets[network.name].find(asset => asset.token === "ETH");
-  const defaultProxyAdmin = await artifacts.readArtifact(
-    "hardhat-deploy/solc_0.8/openzeppelin/proxy/transparent/ProxyAdmin.sol:ProxyAdmin",
-  );
+  const SNAPSHOT_UPDATE_INTERVAL = 24 * 60 * 60;
+
+  // 1.1%
+  const BNBx_ANNUL_GROWTH_RATE = ethers.utils.parseUnits("0.011", 18);
+  let block = await ethers.provider.getBlock("latest");
+  const stakeManagerContract = await ethers.getContractAt("IStaderStakeManager", BNBxStakeManager);
+  let exchangeRate = await stakeManagerContract.convertBnbXToBnb(ethers.utils.parseUnits("1", 18));
+
   await deploy("BNBxOracle", {
     from: deployer,
     log: true,
     deterministicDeployment: false,
-    args: [BNBxStakeManager, BNBx, oracle.address],
-    proxy: {
-      owner: proxyOwnerAddress,
-      proxyContract: "OptimizedTransparentUpgradeableProxy",
-      viaAdminContract: {
-        name: "DefaultProxyAdmin",
-        artifact: defaultProxyAdmin,
-      },
-    },
+    args: [
+      BNBxStakeManager,
+      BNBx,
+      oracle.address,
+      BNBx_ANNUL_GROWTH_RATE,
+      SNAPSHOT_UPDATE_INTERVAL,
+      exchangeRate,
+      block.timestamp,
+    ],
     skipIfAlreadyDeployed: true,
   });
+
+  // 39.21%
+  const slis_BNB_ANNUAL_GROWTH_RATE = ethers.utils.parseUnits("0.3921", 18);
+  block = await ethers.provider.getBlock("latest");
+  const syncClubStakeManagerContract = await ethers.getContractAt("ISynclubStakeManager", slisBNBStakeManager);
+  exchangeRate = await syncClubStakeManagerContract.convertSnBnbToBnb(ethers.utils.parseUnits("1", 18));
 
   await deploy("SlisBNBOracle", {
     from: deployer,
     log: true,
     deterministicDeployment: false,
-    args: [slisBNBStakeManager, slisBNB, oracle.address],
-    proxy: {
-      owner: proxyOwnerAddress,
-      proxyContract: "OptimizedTransparentUpgradeableProxy",
-      viaAdminContract: {
-        name: "DefaultProxyAdmin",
-        artifact: defaultProxyAdmin,
-      },
-    },
+    args: [
+      slisBNBStakeManager,
+      slisBNB,
+      oracle.address,
+      slis_BNB_ANNUAL_GROWTH_RATE,
+      SNAPSHOT_UPDATE_INTERVAL,
+      exchangeRate,
+      block.timestamp,
+    ],
     skipIfAlreadyDeployed: true,
   });
+
+  // 2.3%
+  const stk_BNB_ANNUAL_GROWTH_RATE = ethers.utils.parseUnits("0.023", 18);
+  block = await ethers.provider.getBlock("latest");
+  const stakePoolContract = await ethers.getContractAt("IPStakePool", stkBNBStakePool);
+  const exchangeRateData = await stakePoolContract.exchangeRate();
+  exchangeRate = exchangeRateData.totalWei.mul(ethers.utils.parseUnits("1", 18)).div(exchangeRateData.poolTokenSupply);
 
   await deploy("StkBNBOracle", {
     from: deployer,
     log: true,
     deterministicDeployment: false,
-    args: [stkBNBStakePool, stkBNB, oracle.address],
-    proxy: {
-      owner: proxyOwnerAddress,
-      proxyContract: "OptimizedTransparentUpgradeableProxy",
-      viaAdminContract: {
-        name: "DefaultProxyAdmin",
-        artifact: defaultProxyAdmin,
-      },
-    },
+    args: [
+      stkBNBStakePool,
+      stkBNB,
+      oracle.address,
+      stk_BNB_ANNUAL_GROWTH_RATE,
+      SNAPSHOT_UPDATE_INTERVAL,
+      exchangeRate,
+      block.timestamp,
+    ],
     skipIfAlreadyDeployed: true,
   });
 
   const ankrBNBAddress = ankrBNB || (await ethers.getContract("MockAnkrBNB")).address;
 
+  // 0.7%
   const ankr_BNB_ANNUAL_GROWTH_RATE = ethers.utils.parseUnits("0.007", 18);
-  const ankr_BNB_SNAPSHOT_UPDATE_INTERVAL = 24 * 60 * 60;
-  const block = await ethers.provider.getBlock("latest");
+
+  block = await ethers.provider.getBlock("latest");
   const ankrBNBContract = await ethers.getContractAt("IAnkrBNB", ankrBNBAddress);
-  const exchangeRate = await ankrBNBContract.sharesToBonds(ethers.utils.parseUnits("1", 18));
+  exchangeRate = await ankrBNBContract.sharesToBonds(ethers.utils.parseUnits("1", 18));
 
   await deploy("AnkrBNBOracle", {
     from: deployer,
@@ -86,7 +99,7 @@ const func: DeployFunction = async ({
       ankrBNBAddress,
       oracle.address,
       ankr_BNB_ANNUAL_GROWTH_RATE,
-      ankr_BNB_SNAPSHOT_UPDATE_INTERVAL,
+      SNAPSHOT_UPDATE_INTERVAL,
       exchangeRate,
       block.timestamp,
     ],
@@ -95,19 +108,25 @@ const func: DeployFunction = async ({
 
   const wBETHAddress = wBETH || (await ethers.getContract("MockWBETH")).address;
 
+  // 2.79%
+  const wBETH_ANNUAL_GROWTH_RATE = ethers.utils.parseUnits("0.0279", 18);
+  block = await ethers.provider.getBlock("latest");
+  const wBETHContract = await ethers.getContractAt("IWBETH", wBETHAddress);
+  exchangeRate = await wBETHContract.exchangeRate();
+
   await deploy("WBETHOracle", {
     from: deployer,
     log: true,
     deterministicDeployment: false,
-    args: [wBETHAddress, ETH?.address, oracle.address],
-    proxy: {
-      owner: proxyOwnerAddress,
-      proxyContract: "OptimizedTransparentUpgradeableProxy",
-      viaAdminContract: {
-        name: "DefaultProxyAdmin",
-        artifact: defaultProxyAdmin,
-      },
-    },
+    args: [
+      wBETHAddress,
+      ETH?.address,
+      oracle.address,
+      wBETH_ANNUAL_GROWTH_RATE,
+      SNAPSHOT_UPDATE_INTERVAL,
+      exchangeRate,
+      block.timestamp,
+    ],
     skipIfAlreadyDeployed: true,
   });
 };
