c1deea32f862dcb8b32d654799114755fa7f3ef6
web3rover <arsenal.narayan@gmail.com>
2025-05-02 13:26:25 +0400

    Revert "feat: deployed eBTC oracle"
    This reverts commit 30f9e18e26a09f85f7f507e0ee52950d80120378.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -130,10 +130,7 @@ const config: HardhatUserConfig = {
       chainId: 11155111,
       live: true,
       tags: ["testnet"],
-      // accounts: process.env.DEPLOYER_PRIVATE_KEY ? [`0x${process.env.DEPLOYER_PRIVATE_KEY}`] : [],
-      accounts: {
-        mnemonic: process.env.MNEMONIC || ""
-      }
+      accounts: process.env.DEPLOYER_PRIVATE_KEY ? [`0x${process.env.DEPLOYER_PRIVATE_KEY}`] : [],
     },
     ethereum: {
       url: process.env.ARCHIVE_NODE_ethereum || "https://ethereum.blockpi.network/v1/rpc/public",
