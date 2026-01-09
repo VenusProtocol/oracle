5244422a59eb59c4bb22ec48248dbc5c273f9556
Debugger022 <debugger022@gmail.com>
2026-01-09 19:09:02 +0530

    feat: add PT-clisBNB-25JUN2026 pendle oracle deployments

--- helpers/deploymentConfig.ts
+++ helpers/deploymentConfig.ts
@@ -104,6 +104,7 @@ export const ADDRESSES: PreconfiguredAddresses = {
     USDe: "0x986C30591f5aAb401ea3aa63aFA595608721B1B9",
     xSolvBTC: "0x3ea87323806586A0282b50377e0FEa76070F532B",
     SolvBTC: "0x6855E14A6df91b8E4D55163d068E9ef2530fd4CE",
+    "PT-clisBNB-25JUN2026": "0x60825e8eBbed5C32c1DAA7eA68ceCA70BEA65040",
   },
   bscmainnet: {
     vBNBAddress: mainnetDeployments.contracts.vBNB.address,
